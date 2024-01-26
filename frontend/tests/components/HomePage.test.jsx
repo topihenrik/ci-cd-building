3f80adfc2780e9439a3b76706ab3fac7925f3287
Topi Ilmoni <topi.ilmoni@gmail.com>
2024-01-26 18:29:36 +0200

    Newz

--- frontend/tests/components/HomePage.test.jsx
+++ frontend/tests/components/HomePage.test.jsx
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest';
 import HomePage from "../../src/components/HomePage.jsx";
 import {renderWithProvider} from "../test-utils.jsx";
 
+
 describe('Basic tests', () => {
     it("Should just work", () => {
         expect(1).toBe(1);
